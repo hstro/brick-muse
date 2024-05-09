af78fa2ddee7703318255603009cdfd63bb77f1d
Henry Stromberg <126629864+hstro@users.noreply.github.com>
2024-05-09 07:15:40 -0400

    initial build_5.9.24

---
+++ myscripts.js
@@ -0,0 +1,16 @@
+function ChangeImageA(){
+	document.getElementById('homepage')
+	.src="JT.png";
+}
+function ChangeImageB(){
+	document.getElementById('homepage')
+	.src="AE.webp";
+}
+function ChangeImageC(){
+	document.getElementById('homepage')
+	.src="JB.webp";
+}
+function ResetImage(){
+	document.getElementById('homepage')
+	.src="JR.jpeg";
+}
